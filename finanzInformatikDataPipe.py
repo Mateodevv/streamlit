508b32772fc2df6b620555dd60f220f4f60c1fcf
Mateodevv <74459761+Mateodevv@users.noreply.github.com>
2021-03-02 01:59:55 +0100

    features added.

--- finanzInformatikDataPipe.py
+++ finanzInformatikDataPipe.py
@@ -10,29 +10,33 @@ soup = BeautifulSoup(data, features="html.parser")
 df = pd.DataFrame()
 
 
-
 def find_job_offers():
+    # Gives the div holind all job listings.
     offers = soup.find("div", {"class": re.compile('content-view-children line count-*')})
+    # Will be used for further Data Mining.
     links = []
     job_titles = []
     dates = []
 
-    a = [2, 5, 6]
     for offer in offers.find_all(href=True):
         # Returning found links.
         links.append(offer['href'])
     for offer in offers.find_all("h3"):
-        # replacing the html Tags and adding to list. Returns Job Title of the Inquiry
+        # Replacing the html Tags and adding to list. Returns Job Title of the Inquiry
         job_titles.append(str(offer).replace("<h3>", "").replace("</h3>", ""))
     for offer in offers.find_all("div", {"class": "attribute-publish_date"}):
-        # removing junk from the string. Return Date in format "month.year". [3:] cuts day off from the date.
-        dates.append(
-            str(offer).replace('<div class="attribute-publish_date">', "").replace("</div>", "").replace(" ",
-                                                                                                         "").replace(
-                "\n", "")[3:])
+        # Removing junk from the string. Return Date in format "month.year".
+        fulldate = str(offer).replace('<div class="attribute-publish_date">', "").replace("</div>", "").replace(" ",
+                                                                                                                "").replace(
+            "\n", "")
+
+        # Slicing of the day, reversing and concetanating the string so it fits better for plotting.
+        month = fulldate[3:5]
+        year = fulldate[6:10]
+        dates.append(year + "-" + month)
 
         # Modifies global variable df
         global df
         df = pd.DataFrame(zip(job_titles, dates), columns=["job_title", "date"])
 
-    return links, df
+    return df
--- plot.py
+++ plot.py
@@ -3,29 +3,59 @@ import finanzInformatikDataPipe as fI
 import cluster
 import altair as alt
 
-links, junk = fI.find_job_offers()
+# Init data from Data Pipelines
+junk = fI.find_job_offers()
+bar_chart_df = cluster.get_clustered_job_titles(junk)
+line_chart_df = cluster.get_jobamount_over_time(junk)
+test_df = cluster.get_jobtitles_over_time(junk)
 
-df = cluster.cluster_job_titles(junk)
-st.set_page_config(layout="wide")
-
+# Using whole widthness of the screen with wide mode.
+st.set_page_config(layout="wide", page_title="TrendanalyseFiducia")
+# Splitting the page layout in two columns
 col1, col2 = st.beta_columns(2)
-chart = alt.Chart(df).mark_bar().encode(
+
+bar_chart = alt.Chart(bar_chart_df).mark_bar().encode(
     x='Jobbezeichnung',
     y='offene Stellen',
     color=alt.Color('Jobbezeichnung', legend=alt.Legend(title="Bars by title"))
+).configure_axis(
+    labelFontSize=15,
+    titleFontSize=15
+).configure_legend(
+    labelFontSize=15
 )
 
+base = alt.Chart(test_df).mark_line().encode(
+    x='date',
+    y='amount',
+    color='job_title'
+).configure_axis(
+    labelFontSize=15,
+    titleFontSize=15,
+).configure_legend(
+    labelFontSize=15
+)
+
+line_chart = alt.Chart(line_chart_df).mark_line().encode(
+    x='Datum',
+    y='Anzahl Ausschreibungen'
+).configure_axis(
+    labelFontSize=15,
+    titleFontSize=15
+)
+
+# Elements filling the sidebar
+st.sidebar.subheader("DISCLAIMER")
 st.sidebar.text(
-    "DISCLAIMER: Das ist ein Prototyp zum Thema 'Trendsanalyse durch \nWeb crawling'. Es ist gibt keine Garantie auf"
-    "Richtigkeit der\nDaten. Das Design der Seite kann  sich ebenfalls jederzeit ändern")
+    "Das ist ein Prototyp zum Thema\n'Trendanalyse durch Web crawling'.\nEs gibt keine Garantie auf"
+    " \nRichtigkeit der Daten.")
 
 with col1:
     st.subheader("Offene Stellen in Kategorien sortiert")
-    st.altair_chart(chart, use_container_width=True)
-    st.subheader("Offene Stellen in Kategorien sortiert")
-    st.altair_chart(chart, use_container_width=True)
+    st.altair_chart(bar_chart, use_container_width=True)
+    st.subheader("Offene Stellen über Zeit")
+    st.altair_chart(line_chart, use_container_width=True)
 
 with col2:
-    st.subheader("Offene Stellen in Kategorien sortiert")
-    st.altair_chart(chart, use_container_width=True)
-
+    st.subheader("Offene Stellen gruppiert nach Kategorien über Zeit")
+    st.altair_chart(base, use_container_width=True)
